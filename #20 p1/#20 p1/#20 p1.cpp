039242df622388afeebcea5810a0c6774b03818f
AnasRamadandot <anasqqq35@gmail.com>
2026-03-02 07:40:03 +0300

    Add files via upload

---
+++ #20 p1/#20 p1/#20 p1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	 
+	short x, y;
+	cout << "Pleas enter the first number A =\n";
+	cin >> x;
+	cout << "Pleas enter the first number B =\n";
+	cin >> y;
+	cout << endl;
+
+
+
+ 
+	cout <<"X + Y = " << x + y << endl;
+	cout <<"X - Y = " << x - y << endl;
+	cout << "X * Y = " << x * y << endl;
+	cout << "X / Y = " << x / y << endl;
+	cout << "X % Y = " << x % y << endl;
+
+
+
+
+	return 0;
+
+
+
+
+
+}
